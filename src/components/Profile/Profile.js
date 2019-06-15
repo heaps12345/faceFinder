99559eb929072a37c433db99cd8694fc1762f24a
Dustin heaps <dustinheaps89@gmail.com>
2019-06-15 19:06:35 -0400

    changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -75,71 +75,7 @@ class Profile extends Component {
           </button>
         </form>
       </div>
-      // <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      //   <main className="pa4 black-80">
-      //     <div className="measure">
-      //       <div className="f1 fw6 ph0 mh0">{name.toUpperCase()}</div>
-
-      //       <div className="center">
-      //         <br />
-
-      //         <div className="Tilt-inner pa3">
-      //           <img alt="profilepic" src={`https://robohash.org/${name}?size=150x150`} />
-      //         </div>
-
-      //         <br />
-      //       </div>
-
-      //       <div className="mv3">
-      //         <label className="db fw6 lh-copy f6" htmlFor="password">
-      //           Current Password
-      //         </label>
-      //         <input
-      //           className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-      //           type="password"
-      //           name="cpassword"
-      //           id="cpassword"
-      //           onChange={this.onCurrentPasswordChange}
-      //         />
-      //       </div>
-
-      //       <div className="mv3">
-      //         <label className="db fw6 lh-copy f6" htmlFor="password">
-      //           New Password
-      //         </label>
-      //         <input
-      //           className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-      //           type="password"
-      //           name="npassword"
-      //           id="npassword"
-      //           onChange={this.onNewPasswordChange}
-      //         />
-      //       </div>
-
-      //       <div className="">
-      //         <input
-      //           onClick={this.handleChangePassword}
-      //           className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-      //           type="submit"
-      //           value="Change Password"
-      //         />
-      //       </div>
-
-      //       <br />
-      //       <br />
-      //       <br />
-
-      //       <div className="">
-      //         <input
-      //           onClick={this.handleDeleteProfile}
-      //           className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-      //           type="submit"
-      //           value="Delete Account"
-      //         />
-      //       </div>
-      //     </div>
-      //   </main>
-      // </article>
+      
     );
   }
 }
